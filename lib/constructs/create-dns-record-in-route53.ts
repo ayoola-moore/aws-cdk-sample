b3f865a13b7967ceedebff2108691b0b3d352783
hayoola <onaolapo.ayo@gmail.com>
2023-04-05 10:15:44 +0200

    Update Records as required by Zohomail

--- lib/constructs/create-dns-record-in-route53.ts
+++ lib/constructs/create-dns-record-in-route53.ts
@@ -1,30 +1,45 @@
 import { IDistribution } from "aws-cdk-lib/aws-cloudfront";
 import * as route53 from "aws-cdk-lib/aws-route53";
 import {
-  AaaaRecord,
   ARecord,
+  AaaaRecord,
+  CfnRecordSet,
   CnameRecord,
-  TxtRecord,
 } from "aws-cdk-lib/aws-route53";
 import * as targets from "aws-cdk-lib/aws-route53-targets";
 import { Construct } from "constructs";
 import isSubDomain from "../helpers/isSubDomain";
 
-interface ICreateDNS {
+interface ICreateDNS extends IZohomailRecord {
   domainName: string;
   cloudFront: IDistribution;
-  txtRecordForZohoMail?: string; // Useful for prod case
 }
 
+interface IZohomailRecord {
+  txtRecords?: string[]; // Useful to verify the domain
+  mxRecords?: string[];
+  spfRecord?: string[];
+  dkimRecordName?: string;
+  dkimRecordValue?: string[];
+}
 export default class CreateDNS extends Construct {
   constructor(scope: Construct, id: string, props: ICreateDNS) {
     super(scope, id);
 
-    const { domainName, cloudFront, txtRecordForZohoMail } = props;
+    const HOSTED_ZONE_NAME = "hostedZone";
+
+    const {
+      domainName,
+      cloudFront,
+      txtRecords,
+      mxRecords,
+      dkimRecordName,
+      dkimRecordValue
+    } = props;
 
     const zone = route53.HostedZone.fromHostedZoneAttributes(
       this,
-      "hostedZone",
+      HOSTED_ZONE_NAME,
       {
         zoneName: domainName,
         hostedZoneId: this.node.tryGetContext("hostedZoneId"),
@@ -45,10 +60,34 @@ export default class CreateDNS extends Construct {
       ),
     });
 
-    new TxtRecord(this, "_textRecord", {
-      zone,
-      values: [`${txtRecordForZohoMail}`],
+    // Mx Record: Provided and required by Zohomail
+    mxRecords &&
+      new CfnRecordSet(this, "_mxRecords", {
+        name: domainName,
+        type: route53.RecordType.MX,
+        hostedZoneId: zone.hostedZoneId,
+        ttl: "300",
+        resourceRecords: mxRecords,
+      });
+
+    // All text records who do not require a specify name and defaults to @
+    txtRecords && new CfnRecordSet(this, "_textRecords", {
+      name: domainName,
+      type: "TXT",
+      hostedZoneId: zone.hostedZoneId,
+      ttl: "300",
+      resourceRecords: txtRecords,
     });
+
+    // Txt Records specifically for dkim
+    dkimRecordName &&
+      new CfnRecordSet(this, "dkimTxtRecord", {
+        name: `${dkimRecordName}.${domainName}`,
+        type: "TXT",
+        hostedZoneId: zone.hostedZoneId,
+        ttl: "300",
+        resourceRecords: dkimRecordValue,
+      });
 
     // for apex domain, add a cname record that points for example domain.com to www.domain.com
     isSubDomain(domainName) === false &&
--- lib/easytrip-frontend-stack.ts
+++ lib/easytrip-frontend-stack.ts
@@ -4,7 +4,6 @@ import { Construct } from "constructs";
 import CreateCloudfrontDistributionFromS3 from "./constructs/create-cloudfront-distribution";
 import CreateDNS from "./constructs/create-dns-record-in-route53";
 import CreateStaticHostBucket from "./constructs/create-host-bucket";
-import SetupSSLCert from "./constructs/setup-domain-ssl-certificate";
 import SetIpRestrictionWithWAF from "./constructs/setup-firewall-for-ip-restrictions";
 
 interface IEasytripInfrastructureStack extends StackProps {
@@ -26,7 +25,6 @@ export class EasytripFrontendStack extends Stack {
     const { s3Bucket } = new CreateStaticHostBucket(this, `easytrip-mu-bucket`);
 
     if (deployTo === "dev") {
-      
       // set up Ip restriction using AWS WAF
       const { WAF } = new SetIpRestrictionWithWAF(this, `easytrip-mu-waf`, {
         ipSetId: `ipSet`,
@@ -72,7 +70,15 @@ export class EasytripFrontendStack extends Stack {
       new CreateDNS(this, "dns-prod", {
         domainName,
         cloudFront,
-        txtRecordForZohoMail: "zoho-verification=zb27735349.zmverify.zoho.com", // One time value provided by Zohomail
+        txtRecords: [
+          '"zoho-verification=zb27735349.zmverify.zoho.com"',
+          '"v=spf1 include:zoho.com ~all"',
+        ], // All @ txt records
+        mxRecords: ["10 mx.zoho.com", "20 mx2.zoho.com", "50 mx3.zoho.com"],
+        dkimRecordName: "zmail._domainkey",
+        dkimRecordValue: [
+          '"v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCYX25mkD4G4YRCqGQ+g8Qkcgmc5ET5RU4g/kQgjkhfzTysBgniAfykev3pclzc91pnRTn3fOvMTIwOimlTFl9NcMhoCiSp+RUJZgH9fiVdo6KuAWKsqyLykWdzMI3KBOejT6cBxKdcvPwFau+v+K7ZnhHnbksAn5H9kMGD+lYHuwIDAQAB"',
+        ]
       });
     }
   }
